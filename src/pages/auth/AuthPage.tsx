2e8ae54306bade105b2e2cf1532d71d43c961636
Joaquin <joaquing@usf.edu>
2025-08-12 12:47:44 -0400

    Dulces Momentos 2.4 - Removed home page button

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -14,7 +14,7 @@ const AuthPage = () => {
   };
   
   return (
-    <AuthLayout>
+    <AuthLayout showBackButton={false}>
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Bienvenido a Dulces Momentos
