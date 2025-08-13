c23cad02d79b30457a9bf3554ed82462416b1751
Joaquin <joaquing@usf.edu>
2025-08-13 13:59:17 -0400

    DCT1

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -17,7 +17,7 @@ const AuthPage = () => {
     <AuthLayout showBackButton={false}>
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Bienvenido a Dulces Momentos
+          Bienvenido a Dulces Moment
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Selecciona tu tipo de acceso
